d78fc86845b405fbca911201269e3d30560d2bea
Clarisse de Almeida e Alvarenga <37255795+ClarisseAlvarenga@users.noreply.github.com>
2020-08-16 22:09:27 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,7 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from imblearn.over_sampling import SMOTE
-smote = SMOTE(sampling_strategy='all')
-
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
